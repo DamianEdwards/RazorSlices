6545b49403b9e4d475c087982350b81b5bef53d9
Damian Edwards <damian@damianedwards.com>
2023-03-07 14:24:06 -0800

    Spruce up the sample site

---
+++ samples/RazorSlices.Samples.WebApp/Build/copy-client-libs.js
@@ -0,0 +1,33 @@
+﻿import fs from 'node:fs/promises';
+
+const wwwroot = './wwwroot/lib';
+const libs = {
+    bootstrap: {
+        src: './node_modules/$name/dist',
+        dest: `${wwwroot}/$name/dist`
+    },
+    jquery: {
+        src: './node_modules/$name/dist',
+        dest: `${wwwroot}/$name/dist`
+    }
+};
+
+for (const key in libs) {
+    try {
+        const lib = libs[key],
+            src = lib.src.replace('$name', key),
+            dest = lib.dest.replace('$name', key);
+
+        // delete dest if it already exists
+        try {
+            await fs.rm(dest, { force: true, recursive: true });
+        } catch { }
+
+        // copy src to dest
+        await fs.cp(src, dest, { recursive: true });
+
+        console.log(`Library ${key} copied from ${src} to ${dest}`);
+    } catch (error) {
+        console.error(`Library ${key} could not be copied: ${error}`);
+    }
+}
--- samples/RazorSlices.Samples.WebApp/Program.cs
+++ samples/RazorSlices.Samples.WebApp/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.UseStatusCodePages();
+app.UseStaticFiles();
 
 app.MapGet("/", () => RazorSlice.CreateHttpResult("/Slices/Todos.cshtml", Todos.AllTodos));
 
