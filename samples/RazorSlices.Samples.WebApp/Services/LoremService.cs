ce997364b0b602154aaa47b1a77832c31ec6e38a
MadeYoga <vngla21@gmail.com>
2023-04-02 21:02:57 +0800

    Add a sample using slice with injectable properties

---
+++ samples/RazorSlices.Samples.WebApp/Services/LoremService.cs
@@ -0,0 +1,16 @@
+﻿using RazorSlices.Samples.WebApp.Slices;
+
+namespace RazorSlices.Samples.WebApp.Services;
+
+public class LoremService
+{
+    public string Sentences(int length)
+    {
+        string sentences = "";
+        for (int i = 0; i < length; i++)
+        {
+            sentences += PageContent.Sentence;
+        }
+        return sentences;
+    }
+}
