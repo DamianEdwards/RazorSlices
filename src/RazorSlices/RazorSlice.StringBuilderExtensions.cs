766295ec0cfc91fe556a123c61f5bb4fcb837036
MadeYoga <vngla21@gmail.com>
2023-04-19 15:36:53 +0800

    Add support for rendering slices to a StringBuilder and a string

---
+++ src/RazorSlices/RazorSlice.StringBuilderExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Text.Encodings.Web;
+
+namespace RazorSlices;
+
+/// <summary>
+/// Extension methods for <see cref="RazorSlice" /> that allow rendering to a <see cref="StringBuilder" /> or string.
+/// </summary>
+public static class RazorSliceStringBuilderExtensions
+{
+    /// <summary>
+    /// Renders the template to the specified <see cref="StringBuilder"/>.
+    /// </summary>
+    /// <param name="slice">The <see cref="RazorSlice" /> instance.</param>
+    /// <param name="stringBuilder">The <see cref="StringBuilder"/> to render the template to.</param>
+    /// <param name="htmlEncoder">An optional <see cref="HtmlEncoder"/> instance to use when rendering the template. If none is specified, <see cref="HtmlEncoder.Default"/> will be used.</param>
+    /// <returns>A <see cref="ValueTask"/> representing the rendering of the template.</returns>
+    public static ValueTask RenderAsync(this RazorSlice slice, StringBuilder stringBuilder, HtmlEncoder? htmlEncoder = null)
+    {
+        using var stringWriter = new StringWriter(stringBuilder);
+        return slice.RenderAsync(stringWriter, htmlEncoder);
+    }
+
+    /// <summary>
+    /// Renders the template to a string.
+    /// </summary>
+    /// <param name="slice">The <see cref="RazorSlice" /> instance.</param>
+    /// <param name="htmlEncoder">An optional <see cref="HtmlEncoder"/> instance to use when rendering the template. If none is specified, <see cref="HtmlEncoder.Default"/> will be used.</param>
+    /// <returns>A string of the template.</returns>
+    public static string Render(this RazorSlice slice, HtmlEncoder? htmlEncoder = null)
+    {
+        var sb = new StringBuilder();
+        var task = slice.RenderAsync(sb, htmlEncoder);
+
+        if (task.IsCompletedSuccessfully)
+        {
+            task.GetAwaiter().GetResult();
+        }
+
+        return sb.ToString();
+    }
+}
