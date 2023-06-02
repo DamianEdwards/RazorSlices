fc9fb203c4481763d04f7a69b837e5acbfdf2392
Damian Edwards <damian@damianedwards.com>
2023-06-01 22:12:23 -0700

    Support partials with models.

--- src/RazorSlices/RazorSlice.Partials.cs
+++ src/RazorSlices/RazorSlice.Partials.cs
@@ -27,12 +27,49 @@ public partial class RazorSlice
         if (partialDefinition.HasInjectableProperties && ServiceProvider is null)
         {
             throw new InvalidOperationException($"""
-                The slice '{name}' has @inject properties but the current slice was not provided an IServiceProvider.
+                The template '{name}' has @inject properties but the current slice was not provided an IServiceProvider.
                 You can only render partials with @inject properties from a slice that itself has @inject properties.
                 """);
         }
 
-        var partialSlice = ((SliceFactory)partialDefinition.Factory)();
+        var partialSlice = partialDefinition.HasInjectableProperties
+            ? ((SliceWithServicesFactory)partialDefinition.Factory)(ServiceProvider!)
+            : ((SliceFactory)partialDefinition.Factory)();
+
+        return RenderPartialAsyncImpl(partialSlice);
+    }
+
+    /// <summary>
+    /// Renders a template inline. Call this method from within a template to render another template, e.g.
+    /// <c>@await RenderPartialAsync("/_Footer.cshtml")</c>
+    /// </summary>
+    /// <param name="name">The name of the partial template to render.</param>
+    /// <param name="model">The model to use when rendering the partial template.</param>
+    /// <returns>A <see cref="ValueTask"/> representing the rendering of the template.</returns>
+    /// <exception cref="InvalidOperationException"></exception>
+    protected ValueTask<HtmlString> RenderPartialAsync<TModel>(string name, TModel model)
+    {
+        var partialDefinition = ResolveSliceDefinition(name);
+
+        if (partialDefinition.ModelType is null || !partialDefinition.ModelType.IsAssignableFrom(typeof(TModel)))
+        {
+            throw new InvalidOperationException($"""
+                The template '{name}' requires does not require a model of type '{typeof(TModel).Name}'.
+                Call RenderPartialAsync(string name) to render the template without a model.
+                """);
+        }
+
+        if (partialDefinition.HasInjectableProperties && ServiceProvider is null)
+        {
+            throw new InvalidOperationException($"""
+                The template '{name}' has @inject properties but the current template was not provided an IServiceProvider.
+                You can only render partials with @inject properties from a template that itself has @inject properties.
+                """);
+        }
+
+        var partialSlice = partialDefinition.HasInjectableProperties
+            ? ((SliceWithServicesFactory<TModel>)partialDefinition.Factory)(model, ServiceProvider!)
+            : ((SliceFactory<TModel>)partialDefinition.Factory)(model);
 
         return RenderPartialAsyncImpl(partialSlice);
     }
