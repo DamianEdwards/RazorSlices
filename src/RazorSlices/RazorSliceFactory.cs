f4a3ff5d619f0ced76b85c11d1ca9dfa7d6c4780
Damian Edwards <damian@damianedwards.com>
2023-06-11 19:47:16 -0700

    Remove unused code

--- src/RazorSlices/RazorSliceFactory.cs
+++ src/RazorSlices/RazorSliceFactory.cs
@@ -19,13 +19,6 @@ public static class RazorSliceFactory
 {
     private static readonly HashSet<string> ExcludedServiceNames =
         new(StringComparer.OrdinalIgnoreCase) { "IModelExpressionProvider", "IUrlHelper", "IViewComponentHelper", "IJsonHelper", "IHtmlHelper`1" };
-
-    //private static readonly Type _serviceProviderType = typeof(IServiceProvider);
-    //private static readonly Type _serviceProviderExtensionsType = typeof(ServiceProviderServiceExtensions);
-    //private static readonly MethodInfo _getServiceMethodInfo = _serviceProviderType
-    //    .GetMethod(nameof(IServiceProvider.GetService), new[] { typeof(Type) })!;
-    //private static readonly MethodInfo _getRequiredServiceMethodInfo = _serviceProviderExtensionsType
-    //    .GetMethod(nameof(ServiceProviderServiceExtensions.GetRequiredService), new[] { _serviceProviderType, typeof(Type) })!;
     [DynamicallyAccessedMembers(DynamicallyAccessedMemberTypes.PublicMethods | DynamicallyAccessedMemberTypes.PublicProperties)]
     private static readonly Type _razorSliceType = typeof(RazorSlice);
     private static readonly PropertyInfo _razorSliceInitializeProperty = _razorSliceType.GetProperty(nameof(RazorSlice.Initialize))!;
