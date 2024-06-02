6fd6667bd076558fb8082bbb423c479eda976ae7
Damian Edwards <damian@damianedwards.com>
2024-06-02 15:22:39 -0700

    Move RazorSlice writing methods into separate file

---
+++ src/RazorSlices/RazorSlice.Write.cs
@@ -0,0 +1,341 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Internal;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System.Buffers;
+using System.Globalization;
+
+namespace RazorSlices;
+
+public partial class RazorSlice
+{
+    /// <summary>
+    /// Writes a string value to the output without HTML encoding it.
+    /// </summary>
+    /// <remarks>
+    /// You generally shouldn't call this method directly. The Razor compiler will emit the appropriate calls to this method for
+    /// all blocks of HTML in your .cshtml file.
+    /// </remarks>
+    /// <param name="value">The value to write to the output.</param>
+    protected void WriteLiteral(string? value)
+    {
+        if (string.IsNullOrEmpty(value))
+        {
+            return;
+        }
+
+        _pipeWriter?.WriteHtml(value.AsSpan());
+        _textWriter?.Write(value);
+    }
+
+    /// <summary>
+    /// Writes the string representation of the provided object to the output without HTML encoding it.
+    /// </summary>
+    /// <remarks>
+    /// You generally shouldn't call this method directly. The Razor compiler will emit the appropriate calls to this method for
+    /// all blocks of HTML in your .cshtml file.
+    /// </remarks>
+    /// <param name="value">The value to write to the output.</param>
+    protected void WriteLiteral<T>(T? value)
+    {
+        if (value is IUtf8SpanFormattable)
+        {
+            WriteUtf8SpanFormattable((IUtf8SpanFormattable)(object)value, htmlEncode: false);
+            return;
+        }
+
+        if (value is ISpanFormattable)
+        {
+            WriteSpanFormattable((ISpanFormattable)(object)value, htmlEncode: false);
+            return;
+        }
+
+        WriteLiteral(value?.ToString());
+    }
+
+    /// <summary>
+    /// Writes a buffer of UTF8 bytes to the output without HTML encoding it.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// You generally shouldn't call this method directly. The Razor compiler will emit the appropriate calls to this method for
+    /// all blocks of HTML in your .cshtml file.
+    /// </para>
+    /// <para>
+    /// NOTE: We'd need a tweak to the Razor compiler to to have it support emitting <see cref="WriteLiteral(ReadOnlySpan{byte})"/> calls with UTF8 string literals
+    ///       i.e. https://github.com/dotnet/razor/issues/8429
+    /// </para>
+    /// </remarks>
+    /// <param name="value">The value to write to the output.</param>
+    protected void WriteLiteral(ReadOnlySpan<byte> value)
+    {
+        if (value.Length == 0)
+        {
+            return;
+        }
+
+        _pipeWriter?.Write(value);
+        _textWriter?.WriteUtf8(value);
+    }
+
+    /// <summary>
+    /// Writes a <see cref="bool"/> value to the output.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// You generally shouldn't call this method directly. The Razor compiler will emit the appropriate calls to this method for
+    /// all matching Razor expressions in your .cshtml file.
+    /// </para>
+    /// <para>
+    /// To manually write out a value, use <see cref="WriteBool"/> instead, e.g. <c>@WriteBool(todo.Complete)</c>
+    /// </para>
+    /// </remarks>
+    /// <param name="value"></param>
+    protected void Write(bool? value) => WriteBool(value);
+
+    /// <summary>
+    /// Writes a buffer of UTF8 bytes to the output after HTML encoding it.
+    /// </summary>
+    /// <remarks>
+    /// You generally shouldn't call this method directly. The Razor compiler will emit the appropriate calls to this method for
+    /// all matching Razor expressions in your .cshtml file.
+    /// </remarks>
+    /// <param name="value">The value to write to the output.</param>
+    protected void Write(byte[] value) => Write(value.AsSpan());
+
+    /// <summary>
+    /// Writes a buffer of UTF8 bytes to the output after HTML encoding it.
+    /// </summary>
+    /// <remarks>
+    /// You generally shouldn't call this method directly. The Razor compiler will emit the appropriate calls to this method for
+    /// all matching Razor expressions in your .cshtml file.
+    /// </remarks>
+    /// <param name="value">The value to write to the output.</param>
+    protected void Write(ReadOnlySpan<byte> value)
+    {
+        if (value.Length == 0)
+        {
+            return;
+        }
+
+        _pipeWriter?.HtmlEncodeAndWriteUtf8(value, _htmlEncoder);
+        _textWriter?.HtmlEncodeAndWriteUtf8(value, _htmlEncoder);
+    }
+
+    /// <summary>
+    /// Writes the specified <see cref="HtmlString"/> value to the output without HTML encoding it again.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// You generally shouldn't call this method directly. The Razor compiler will emit the appropriate calls to this method for
+    /// all matching Razor expressions in your .cshtml file.
+    /// </para>
+    /// <para>
+    /// To manually write out a value, use <see cref="WriteHtml{T}(T)"/> instead,
+    /// e.g. <c>@WriteHtml(myCustomHtmlString)</c>
+    /// </para>
+    /// </remarks>
+    /// <param name="htmlString">The <see cref="HtmlString"/> value to write to the output.</param>
+    protected void Write(HtmlString htmlString)
+    {
+        if (htmlString is not null && htmlString != HtmlString.Empty)
+        {
+            WriteHtml(htmlString);
+        }
+    }
+
+    /// <summary>
+    /// Writes the specified <see cref="IHtmlContent"/> value to the output without HTML encoding it again.
+    /// </summary>
+    /// <param name="htmlContent">The <see cref="IHtmlContent"/> value to write to the output.</param>
+    protected void Write(IHtmlContent? htmlContent)
+    {
+        if (htmlContent is not null)
+        {
+            WriteHtml(htmlContent);
+        }
+    }
+
+    /// <summary>
+    /// Writes the specified value to the output after HTML encoding it.
+    /// </summary>
+    /// <param name="value">The value to write to the output.</param>
+    protected void Write(string? value)
+    {
+        if (!string.IsNullOrEmpty(value))
+        {
+            Write(value.AsSpan());
+        }
+    }
+
+    /// <summary>
+    /// Writes the specified value to the output after HTML encoding it.
+    /// </summary>
+    /// <param name="value">The value to write to the output.</param>
+    protected void Write(ReadOnlySpan<char> value)
+    {
+        if (value.Length > 0)
+        {
+            _pipeWriter?.HtmlEncodeAndWrite(value, _htmlEncoder);
+            _textWriter?.HtmlEncodeAndWrite(value, _htmlEncoder);
+        }
+    }
+
+    /// <summary>
+    /// Writes the specified <typeparamref name="T"/> to the output.
+    /// </summary>
+    /// <remarks>
+    /// You generally shouldn't call this method directly. The Razor compiler will emit calls to the most appropriate overload of
+    /// the <c>Write</c> method for all Razor expressions in your .cshtml file, e.g. <c>@someVariable</c>.
+    /// </remarks>
+    /// <param name="value">The <typeparamref name="T"/> to write to the output.</param>
+    protected void Write<T>(T? value)
+    {
+        WriteValue(value);
+    }
+
+    /// <summary>
+    /// Writes a <see cref="bool"/> value to the output.
+    /// </summary>
+    /// <param name="value">The value to write to the output.</param>
+    /// <returns><see cref="HtmlString.Empty"/> to allow for easy calling via a Razor expression, e.g. <c>@WriteBool(todo.Completed)</c></returns>
+    protected HtmlString WriteBool(bool? value)
+    {
+        if (value.HasValue)
+        {
+            _pipeWriter?.Write(value.Value);
+            _textWriter?.Write(value.Value);
+        }
+        return HtmlString.Empty;
+    }
+
+    /// <summary>
+    /// Write the specified <see cref="ISpanFormattable"/> value to the output with the specified format and optional <see cref="IFormatProvider" />.
+    /// </summary>
+    /// <param name="formattable">The value to write to the output.</param>
+    /// <param name="format">The format to use when writing the value to the output. Defaults to the default format for the value's type if not provided.</param>
+    /// <param name="formatProvider">The <see cref="IFormatProvider" /> to use when writing the value to the output. Defaults to <see cref="CultureInfo.CurrentCulture"/> if <c>null</c>.</param>
+    /// <param name="htmlEncode">Whether to HTML encode the value or not. Defaults to <c>true</c>.</param>
+    /// <returns><see cref="HtmlString.Empty"/> to allow for easy calling via a Razor expression, e.g. <c>@WriteSpanFormattable(item.DueBy, "d")</c></returns>
+    protected HtmlString WriteSpanFormattable<T>(T? formattable, ReadOnlySpan<char> format = default, IFormatProvider? formatProvider = null, bool htmlEncode = true)
+        where T : ISpanFormattable
+    {
+        if (formattable is not null)
+        {
+            var htmlEncoder = htmlEncode ? _htmlEncoder : NullHtmlEncoder.Default;
+            _pipeWriter?.HtmlEncodeAndWriteSpanFormattable(formattable, htmlEncoder, format, formatProvider);
+            _textWriter?.HtmlEncodeAndWriteSpanFormattable(formattable, htmlEncoder, format, formatProvider);
+        }
+
+        return HtmlString.Empty;
+    }
+
+    /// <summary>
+    /// Write the specified <see cref="IUtf8SpanFormattable"/> value to the output with the specified format and optional <see cref="IFormatProvider" />.
+    /// </summary>
+    /// <param name="formattable">The value to write to the output.</param>
+    /// <param name="format">The format to use when writing the value to the output. Defaults to the default format for the value's type if not provided.</param>
+    /// <param name="formatProvider">The <see cref="IFormatProvider" /> to use when writing the value to the output. Defaults to <see cref="CultureInfo.CurrentCulture"/> if <c>null</c>.</param>
+    /// <param name="htmlEncode">Whether to HTML encode the value or not. Defaults to <c>true</c>.</param>
+    /// <returns><see cref="HtmlString.Empty"/> to allow for easy calling via a Razor expression, e.g. <c>@WriteUtf8SpanFormattable(item.DueBy, "d")</c></returns>
+    protected HtmlString WriteUtf8SpanFormattable<T>(T? formattable, ReadOnlySpan<char> format = default, IFormatProvider? formatProvider = null, bool htmlEncode = true)
+        where T : IUtf8SpanFormattable
+    {
+        if (formattable is not null)
+        {
+            var htmlEncoder = htmlEncode ? _htmlEncoder : NullHtmlEncoder.Default;
+            _pipeWriter?.HtmlEncodeAndWriteUtf8SpanFormattable(formattable, htmlEncoder, format, formatProvider);
+            _textWriter?.HtmlEncodeAndWriteUtf8SpanFormattable(formattable, htmlEncoder, format, formatProvider);
+        }
+
+        return HtmlString.Empty;
+    }
+
+    /// <summary>
+    /// Writes the specified <see cref="IHtmlContent"/> value to the output.
+    /// </summary>
+    /// <param name="htmlContent">The <see cref="IHtmlContent"/> value to write to the output.</param>
+    /// <returns><see cref="HtmlString.Empty"/> to allow for easy calling via a Razor expression, e.g. <c>@WriteHtmlContent(myCustomHtmlContent)</c></returns>
+    protected HtmlString WriteHtml<T>(T htmlContent)
+        where T : IHtmlContent
+    {
+        if (htmlContent is not null)
+        {
+            if (_pipeWriter is not null)
+            {
+                _utf8BufferTextWriter ??= Utf8PipeTextWriter.Get(_pipeWriter);
+                htmlContent.WriteTo(_utf8BufferTextWriter, _htmlEncoder);
+            }
+            if (_textWriter is not null)
+            {
+                htmlContent.WriteTo(_textWriter, _htmlEncoder);
+            }
+        }
+
+        return HtmlString.Empty;
+    }
+
+    /// <summary>
+    /// Writes the specified <see cref="HtmlString"/> value to the output.
+    /// </summary>
+    /// <param name="htmlString">The <see cref="HtmlString"/> value to write to the output.</param>
+    /// <returns><see cref="HtmlString.Empty"/> to allow for easy calling via a Razor expression, e.g. <c>@WriteHtml(myCustomHtmlContent)</c></returns>
+    protected HtmlString WriteHtml(HtmlString htmlString)
+    {
+        if (htmlString is not null && htmlString != HtmlString.Empty)
+        {
+            _pipeWriter?.WriteHtml(htmlString.Value);
+            _textWriter?.Write(htmlString.Value);
+        }
+
+        return HtmlString.Empty;
+    }
+
+    private void WriteValue<T>(T value)
+    {
+        if (value is null)
+        {
+            return;
+        }
+
+        // Dispatch to the most appropriately typed method
+        if (TryWriteFormattableValue(value))
+        {
+            return;
+        }
+
+        if (value is string)
+        {
+            Write((string)(object)value);
+        }
+        else if (value is byte[])
+        {
+            Write(((byte[])(object)value).AsSpan());
+        }
+        // Handle derived types (this currently results in value types being boxed)
+        else if (value is IUtf8SpanFormattable)
+        {
+            WriteUtf8SpanFormattable((IUtf8SpanFormattable)(object)value, default, null);
+        }
+        else if (value is ISpanFormattable)
+        {
+            WriteSpanFormattable((ISpanFormattable)(object)value, default, null);
+        }
+        else if (value is HtmlString)
+        {
+            WriteHtml((HtmlString)(object)value);
+        }
+        else if (value is IHtmlContent)
+        {
+            WriteHtml((IHtmlContent)(object)value);
+        }
+        else if (value is Enum)
+        {
+            WriteSpanFormattable((Enum)(object)value);
+        }
+        // Fallback to ToString()
+        else
+        {
+            Write(value?.ToString());
+        }
+    }
+}
