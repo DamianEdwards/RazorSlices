1c4cd67be9ec4de9622905015a7e39bfb4732e1e
Damian Edwards <damian@damianedwards.com>
2025-07-28 16:54:27 -0700

    Tweak MSBuild targets & generator
    Fixes #94
    Fixes #85
    Fixes #66
    Related to #85

---
+++ samples/RazorSlices.Samples.RazorClassLibrary/Slices/FromLibrary.cshtml.cs
@@ -0,0 +1,14 @@
+﻿namespace RazorSlices.Samples.RazorClassLibrary.Slices;
+
+public partial class FromLibrary
+{
+    public class Model
+    {
+        public required string Message { get; init; }
+    }
+
+    public static async Task<string> ExampleCustomRenderAsync(Model model)
+    {
+        return await Create(model).RenderAsync();
+    }
+}
