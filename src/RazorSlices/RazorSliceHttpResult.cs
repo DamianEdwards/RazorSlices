61a2c73c0f6f55c554afb69a0b102216480f8016
Damian Edwards <damian@damianedwards.com>
2026-02-16 17:59:21 -0800

    Remove other uses of RazorSliceHttpResult

--- src/RazorSlices/RazorSliceHttpResult.cs
+++ src/RazorSlices/RazorSliceHttpResult.cs
@@ -6,7 +6,7 @@ namespace Microsoft.AspNetCore.Http.HttpResults;
 /// A <see cref="RazorSlice" /> template that is also an <see cref="IResult" /> so it can be directly returned from
 /// a route handler delegate. When executed it will render the template to the response.
 /// </summary>
-[Obsolete("This class is no longer needed. RazorSlice itself implements IResult and can be returned directly from route handlers.")]
+[Obsolete("This class is no longer needed. RazorSlice itself implements IResult.")]
 public abstract class RazorSliceHttpResult : RazorSlice
 {
     
